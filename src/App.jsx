ca2aa5ef32723103ccb80a0b9597a295de4c76ed
Ramon Faria <raborzoni@gmail.com>
2025-02-13 19:24:04 -0300

    Devido alguns erros no deploy para a vercel, foi movido de lugar o arquivo Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import Home from './Home.jsx';
 import Consultar from './pages/Consultar.jsx';
 import CriarDemanda from './pages/CriarDemanda.jsx';
 import AtualizarDemanda from './pages/AtualizarDemanda.jsx';
