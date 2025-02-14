1bfbaff6d4b88931ddd49f3f2039e5b53b3bb4fb
Ramon Faria <raborzoni@gmail.com>
2025-02-14 17:02:27 -0300

    Estilização da tela inicial.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../style/home.css';
+import logo from '../assets/cropped-LOGO_CAPESESP-1400x1016.png';
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="container">
-      <h1 className="title">Sistema de Demandas</h1>
+    <div className="homeContainer">
+      <div className="logo-container">
+        <img src={logo} alt="Logo do Sistema" className="logo" />
+      </div>
+
+      <div className="demanda-home-tittle-container">
+        <h1 className="title">Sistema de Demandas</h1>
+      </div>
+
       <div className="buttonContainer">
         <button onClick={() => navigate('/consultar')} className="button">
           Consultar
