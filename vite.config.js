6bd1b9c62556ac397435e7f9af1ea02f4c88e3ba
Ramon Faria <raborzoni@gmail.com>
2025-02-17 11:33:37 -0300

    Segundo teste para deploy na vercel.

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
