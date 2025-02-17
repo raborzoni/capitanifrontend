28acb5b6f2ced04bab9ba156c2fe6ace98637c09
Ramon Faria <raborzoni@gmail.com>
2025-02-17 11:31:44 -0300

    Teste de deploy na Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
-import Consultar from './pages/Consultar.jsx';
-import CriarDemanda from './pages/CriarDemanda.jsx';
-import AtualizarDemanda from './pages/AtualizarDemanda.jsx';
+import Consultar from '../src/pages/Consultar.jsx';
+import CriarDemanda from '../src/pages/CriarDemanda.jsx';
+import AtualizarDemanda from '../src/pages/AtualizarDemanda.jsx';
 
 const App = () => {
   return (
