4a021672400caf33394833e4abcaad59feb3883c
Ramon Faria <raborzoni@gmail.com>
2025-02-17 11:42:35 -0300

    Mudança de pasta dos arquivos do sistema.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import Home from './views/Home.jsx';
 import Consultar from '@/views/Consultar.jsx';
 import CriarDemanda from '@/views/CriarDemanda.jsx';
 import AtualizarDemanda from '@/views/AtualizarDemanda.jsx';
