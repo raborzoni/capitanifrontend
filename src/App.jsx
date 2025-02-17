69ed9f926085ffc204c15d40bfbebe92f3e31781
Ramon Faria <raborzoni@gmail.com>
2025-02-17 11:40:22 -0300

    Refazendo o teste de deploy.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
-import Consultar from '@/views/Consultar';
+import Consultar from '@/views/Consultar.jsx';
 import CriarDemanda from '@/pages/CriarDemanda.jsx';
 import AtualizarDemanda from '@/pages/AtualizarDemanda.jsx';
 
